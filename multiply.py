958dc0dc854afb0894c6a7b3e3b5251e5231b16b
Alisha <alisha.rai@heraldcollege.edu.np>
2025-03-18 20:51:58 +0545

    multiplication code

---
+++ multiply.py
@@ -0,0 +1,6 @@
+# Simple CLI Multiplication Calculator
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 * num2
+print(f"✅ The product is: {result}")

c67b3e4aa2697422803bf4ed5940a6d7eee9c1f9
Bhanu Aryal <bhanuaryal001@gmail.com>
2025-03-18 20:59:27 +0545

    Multiplication Code Fix.

--- multiply.py
+++ multiply.py
@@ -2,6 +2,7 @@ def multiply_numbers(num1: float, num2: float) -> float:
     """Returns the product of two numbers."""
     return num1 * num2
 
+
 def main():
     try:
         num1 = float(input("Enter first number: "))
